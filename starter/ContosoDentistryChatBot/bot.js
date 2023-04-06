4118d06e8fad046eee099a317a547d79b8098722
Trong.Phan@coxautoinc.com <Trong.Phan@coxautoinc.com>
2023-04-06 21:32:49 +0700

    second commit

--- starter/ContosoDentistryChatBot/bot.js
+++ starter/ContosoDentistryChatBot/bot.js
@@ -17,19 +17,20 @@ class DentaBot extends ActivityHandler {
         this.QnAMaker = new QnAMaker()
        
         // create a DentistScheduler connector
-      
-        // create a IntentRecognizer connector
+        this.dentistScheduler = new DentistScheduler(configuration.SchedulerConfiguration);
 
+        // create a LUIS connector
+        this.intentRecognizer = new IntentRecognizer(configuration.LuisConfiguration);
 
         this.onMessage(async (context, next) => {
             // send user input to QnA Maker and collect the response in a variable
             // don't forget to use the 'await' keyword
+            const qnaResults = await this.qnaMaker.getAnswers(context);
           
             // send user input to IntentRecognizer and collect the response in a variable
             // don't forget 'await'
-                     
+            const LuisResult = await this.intentRecognizer.executeLuisQuery(context);                     
             // determine which service to respond with based on the results from LUIS //
-
             // if(top intent is intentA and confidence greater than 50){
             //  doSomething();
             //  await context.sendActivity();
@@ -37,6 +38,32 @@ class DentaBot extends ActivityHandler {
             //  return;
             // }
             // else {...}
+            if (LuisResult.luisResult.prediction.topIntent === "getAvailability" &&
+                LuisResult.intents.getAvailability.score > .6 &&
+                LuisResult.entities.$instance && 
+                LuisResult.entities.$instance.timeday  && 
+                LuisResult.entities.$instance.timeday[0]
+            ) {
+                const timeday = LuisResult.entities.$instance.timeday[0].text;
+
+                const getAvailableTime = "I have a few spots for " + timeday;
+                console.log(getAvailableTime)
+                
+                await context.sendActivity(getAvailableTime);
+                await next();
+                return;
+            } 
+
+
+            if (qnaResults[0]) {
+                console.log(qnaResults[0])
+                await context.sendActivity(`${qnaResults[0].answer}`);
+            }
+            else {
+                await context.sendActivity(`I'm not sure`
+                + 'I found an answer to your question'
+                + `You can ask me questions about our dental services like "Do you treat children?' or tell me what time you want an appointment like "today"`);
+            }
              
             await next();
     });
@@ -44,7 +71,7 @@ class DentaBot extends ActivityHandler {
         this.onMembersAdded(async (context, next) => {
         const membersAdded = context.activity.membersAdded;
         //write a custom greeting
-        const welcomeText = '';
+        const welcomeText = 'Hello and welcome!';
         for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
             if (membersAdded[cnt].id !== context.activity.recipient.id) {
                 await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
